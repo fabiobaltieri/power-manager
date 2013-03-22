bdd602452bd92077498b698cfb890490736a2f9a
Fabio Baltieri <fabio.baltieri@gmail.com>
2013-03-22 21:33:05 +0100

    firmware: update license headers on C source files

--- firmware/adc.c
+++ firmware/adc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Fabio Baltieri (fabio.baltieri@gmail.com)
+ * Copyright 2013 Fabio Baltieri (fabio.baltieri@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
